fc93d0982d3ba1e45918a326abb4919fb3093d74
Muhammad <muhammmmadazizekubov@gmail.com>
2025-05-27 18:43:07 +0500

    change profile to category

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AdminModule } from './admin/admin.module';
 import { UsersModule } from './users/users.module';
 import { UserProfileModule } from './user-profile/user-profile.module';
 import { ProductModule } from './product/product.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [AdminModule, UsersModule, UserProfileModule, ProductModule],
+  imports: [AdminModule, UsersModule, UserProfileModule, ProductModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
