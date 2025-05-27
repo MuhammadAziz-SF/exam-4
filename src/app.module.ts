c17145baf94bfb22480081125c761f0a28b11a61
Muhammad <muhammmmadazizekubov@gmail.com>
2025-05-27 17:30:33 +0500

    admin, user, user-profile, product empty crud created

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
+import { UserProfileModule } from './user-profile/user-profile.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [AdminModule, UsersModule, UserProfileModule, ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
